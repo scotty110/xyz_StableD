83abdaea4d549b71d7d54efa011fadf5fd0d1247
scotty110 <none@none>
2023-02-19 13:50:38 -0500

    working

---
+++ client_server/cmd/client/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "bufio"
+    "context"
+    "fmt"
+    //"log"
+    "os"
+    "net/http"
+    "strings"
+
+    pb "golang.a2z.com/stableDiffusion/rpc"
+)
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter text: ")
+    text, _ := reader.ReadString('\n')
+    inputString := &pb.String{Text: text}
+
+    // Create a client capable of talking to server
+    // Copied from twitch
+    client := pb.NewText2ImageProtobufClient("http://localhost:8080", &http.Client{})
+    var (
+        sd *pb.Bytes
+        err error
+    )
+
+    sd, err = client.DiffusionModel(context.Background(), inputString)
+    if err != nil {
+        fmt.Println("Stable Diffusion Errored out")
+        fmt.Println(sd)
+    } else {
+        fmt.Println("Stable Diffusion Ran, Probably")
+    }
+
+    // Write to file
+    fname := strings.Replace(inputString.Text, " ", "_", -1)
+    fname = fmt.Sprintf("%s.png", strings.Replace(fname, "\n","", -1))
+    f, err := os.Create(fname)
+    if err != nil {
+        panic(err)
+    } else {
+        f.Write(sd.Bytes)
+    }
+}
